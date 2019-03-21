129a8805ee840e787876811a9ee7e3be325704c4
1846689910 <zuhe75@gmail.com>
2019-03-21 00:00:39 -0700

    test wip

--- test/coder.spec.js
+++ test/coder.spec.js
@@ -1,17 +1,25 @@
-const config = require("../src/config");
+const Path = require("path");
 const { expect } = require("chai");
 const sinon = require("sinon");
+const Fs = require("fs");
+/**
+ * 1 自定义的需要有方法或属性被stub的module，和需要被test的并且使用前者module的module最好都能在自己的case中require，否则可能会无法正确替换
+ * 2 需要被测试的module需要在stub之后再引入
+ * 3 有时在每次测试后清空reqire.cache是必要的
+ */
 describe("coder", () => {
+  let fakeConfig;
   describe("function resolveKey", () => {
-    let fakeConfig;
     afterEach(() => {
       if (fakeConfig) {
         fakeConfig.restore();
         fakeConfig = null;
       }
+      delete process.env.CODER_KEY;
+      Object.keys(require.cache).forEach(key => delete require.cache[key]);
     });
     it("should use config.KEY if no CODER_KEY given", () => {
-      fakeConfig = sinon.stub(config, "KEY").value("key");
+      fakeConfig = sinon.stub(require("../src/config"), "KEY").value("key");
       const { resolveKey } = require("../src/coder"); // needs to load self-defined module after stub, otherwise cannot stub property inside
       const key = resolveKey();
       expect(key).to.equal("key");
@@ -21,6 +29,39 @@ describe("coder", () => {
       const { resolveKey } = require("../src/coder");
       const key = resolveKey();
       expect(key).to.equal("123");
+    });
+    it("should throw error if no KEY in config and no CODER_KEY in process.env", () => {
+      fakeConfig = sinon.stub(require("../src/config"), "KEY").value(undefined);
+      const { resolveKey } = require("../src/coder");
+      expect(resolveKey).to.throw();
+    });
+  });
+  describe("function decode", () => {
+    const decodedPath = Path.resolve("test", "__dist", "a.txt");
+    const encodedPath = Path.resolve("test", "__lib", "a.encoded.txt");
+    before(() => {
+      const _config = require("../src/config");
+      fakeConfig = sinon.stub(_config, "KEY").value("key");
+      fakeConfig = sinon.stub(_config, "DECODED_PATH").value(decodedPath);
+      fakeConfig = sinon.stub(_config, "ENCODED_PATH").value(encodedPath);
+    });
+    after(() => {
+      if (fakeConfig) {
+        fakeConfig.restore();
+        fakeConfig = null;
+      }
+    });
+    afterEach(() => {
+      Object.keys(require.cache).forEach(key => delete require.cache[key]);
+      if (Fs.existsSync(encodedPath)) Fs.unlink(encodedPath);
+      if (Fs.existsSync(decodedPath)) Fs.unlink(decodedPath);
+    });
+    it("should throw error when encodedPath does not exist", async () => {
+      const { decode } = require("../src/coder");
+      let err;
+      await decode().catch(e => (err = e));
+      expect(err).to.exist;
+      expect(err.message).to.equal(`${encodedPath} is missing`);
     });
   });
 });
