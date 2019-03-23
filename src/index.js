61cca0dac967e837d7aa106266173f218548bfca
1846689910 <zuhe75@gmail.com>
2019-03-23 00:18:56 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 const { encode, decode } = require("./coder");
-
-const getTask = () => {
-  const task = process.argv.slice(2).find(x => {
-    const s = x.toUpperCase();
-    return s === "--DECODE" || s === "--ENCODE";
-  });
-  if (task) {
-    return task.toUpperCase();
-  } else {
-    throw new Error("Task Mode Missing: --decode or --encode");
-  }
-};
+const { getTask, getFrom, getTo } = require("./utils");
 
 const main = async () => {
   const task = getTask();
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+function getTask() {
+  const task = process.argv.slice(2).find(x => {
+    const s = x.toUpperCase();
+    return s === "--DECODE" || s === "--ENCODE";
+  });
+  if (task) {
+    return task.toUpperCase();
+  } else {
+    throw new Error("Task Mode Missing: --decode or --encode");
+  }
+}
+
+function getFrom() {
+  const from = process.argv.slice(2).find(x => x.toUpperCase().startsWith("--FROM"));
+  return from && from.split("=")[1];
+}
+
+function getTo() {
+  const to = process.argv.slice(2).find(x => x.toUpperCase().startsWith("--TO"));
+  return to && to.split("=")[1];
+}
+
+module.exports = {
+  getTask,
+  getFrom,
+  getTo
+};
