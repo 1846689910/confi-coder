2a3c4b736519eb45d4d2c52fdf5b5e8f681c46e9
1846689910 <zuhe75@gmail.com>
2019-03-24 10:58:15 -0700

    update
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ..

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Path = require("path");
 module.exports = {
-  ENCODED_PATH: Path.resolve("lib", "concise.ecd"),
+  ENCODED_PATH: Path.resolve("lib", "concise.html.ecd"),
   DECODED_PATH: Path.resolve("dist", "concise.html"),
   KEY: "you never know"
 };
